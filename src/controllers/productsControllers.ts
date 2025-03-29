73a7d9082cf926cd1431ccac364cfde33f9b9c8f
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2025-03-29 19:27:20 +0300

    add simple pagination

--- src/controllers/productsControllers.ts
+++ src/controllers/productsControllers.ts
@@ -56,6 +56,9 @@ const productsControllers = {
   getProductByCategory: async (req: Request, res: Response) => {
     const { category } = req.query;
     const language = req.query.language?.toString() || "en";
+    const page = parseInt(req.query.page?.toString() || "1");
+    const limit = parseInt(req.query.limit?.toString() || "16"); 
+
     try {
       let query = {};
       if (category === "all") {
@@ -65,8 +68,12 @@ const productsControllers = {
         query = { category: { $regex: new RegExp(category as string, "i") } };
       }
 
-      const products = await Product.find(query);
-      const localizedProducts = products.map((product) => {
+        const totalProducts = await Product.countDocuments(query);
+        const products = await Product.find(query)
+          .skip((page - 1) * limit)
+          .limit(limit);
+
+        const localizedProducts = products.map((product) => {
         const localizedName = isValidLanguage(language) ? product.name[language] : product.name.en;
 
         const localizedDescription = isValidLanguage(language) ? product.description[language] : product.description.en;
@@ -81,8 +88,16 @@ const productsControllers = {
         };
       });
 
-      res.json(localizedProducts);
-    } catch (error) {
+      res.json({
+        products: localizedProducts,
+        pagination: {
+          total: totalProducts,
+          page,
+          limit,
+          pages: Math.ceil(totalProducts / limit)
+        }
+      });
+      } catch (error) {
       console.error("Error fetching products:", error);
       res.status(500).json({ error: "Internal server error" });
     }
